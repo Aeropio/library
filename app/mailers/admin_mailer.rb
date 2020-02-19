fc5ab302bfc07a639f8e0de900b23930ea121f57
Aeropio <azramasood369@gmail.com>
2020-02-19 07:13:13 +0530

    removed byebug

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,7 +3,6 @@ class AdminMailer < ApplicationMailer
    
     def notify_admin_email
       @admin = User.where(is_admin: true).last
-      byebug
       #@url  = 'http://example.com/login'
       mail(to: @admin.email, subject: 'Welcome to My Awesome Site')
     end
