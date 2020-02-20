2dcf0b81cca7fad20600ff67648e98aff8182d7d
Aeropio <azramasood369@gmail.com>
2020-02-20 16:42:00 +0530

    env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
- :user_name            => 'azra.m@seanergydigital.com',
- :password             => 'Qwerfty()&685NhSSS',
+ :user_name            => ENV['EMAIL'],
+ :password             => ENV['PASSWORD'],
  :authentication       => "plain",
 :enable_starttls_auto => true
 }
