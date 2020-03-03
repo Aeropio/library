a0519e70013f9c006db299754e22a043b40a197d
Aeropio <azramasood369@gmail.com>
2020-03-03 16:31:09 +0530

    last changes for demo hopelu

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -4,16 +4,18 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
+  include Cloudinary::CarrierWave
+
   #process resize_to_limit: [3000, 3000]
 
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   def extension_white_list
     %w(jpg jpeg gif png)
